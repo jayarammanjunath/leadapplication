2a3c920a9e7f857a8852ceedb482127f221bc362
jayappamanju <jayarammanju777@gmail.com>
2023-04-01 14:32:55 +0530

    updating Application for HIS-300

--- src/main/java/com/example/leadapp/LeadApplication.java
+++ src/main/java/com/example/leadapp/LeadApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeadApplication {
+	HashMap<Integer,String> map=new HashMap<Integer,String>();
 	public static void main(String[] args) {
 		SpringApplication.run(LeadApplication.class, args);
 	}
