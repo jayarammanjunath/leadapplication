cd0cc896acfe01eda373d4349c7a6eac3bb8f34b
jayarammanjunath <125115634+jayarammanjunath@users.noreply.github.com>
2023-04-01 14:07:36 +0530

    Update LeadApplication.java

--- src/main/java/com/example/leadapp/LeadApplication.java
+++ src/main/java/com/example/leadapp/LeadApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeadApplication {
-     //this is ram
-	//this is venkat
-	//this is manju
 	public static void main(String[] args) {
 		SpringApplication.run(LeadApplication.class, args);
 	}
